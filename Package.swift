8c467d02679d6ebca680a4a91ee8ec74b7dce7b3
anplex <ssp_tech@carty.io>
2026-01-19 19:38:14 +0800

    Update Package.swift

--- Package.swift
+++ Package.swift
@@ -13,6 +13,10 @@ let package = Package(
             targets: ["CartyAdmobAdapter"]
         ),
     ],
+    dependencies: [
+        .package(url:"https://github.com/cartysdk/CartyAdmobAdapter-iOS.git", from: "0.6.0"),
+        .package(url:"https://github.com/googleads/swift-package-manager-google-mobile-ads.git", from: "0.0.1"),
+    ],
     targets: [
         .target(
             name: "CartyAdmobAdapter",
