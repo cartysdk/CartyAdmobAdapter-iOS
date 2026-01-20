5c6ab1c9bc2f9b7ac4029e635b110546afa4aa16
anplex <ssp_tech@carty.io>
2026-01-20 10:30:37 +0800

    Update Package.swift

--- Package.swift
+++ Package.swift
@@ -14,7 +14,7 @@ let package = Package(
         ),
     ],
     dependencies: [
-        .package(url:"https://github.com/cartysdk/Carty-swift-package-manager", from: "0.6.0")
+        .package(url: "https://github.com/cartysdk/Carty-swift-package-manager.git", from: "0.6.0")
     ],
     targets: [
         .target(
